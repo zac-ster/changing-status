04817e822eb86fc26db4f25a74cb0562d4e934f0
ZacBotDev <60801799+zac-ster@users.noreply.github.com>
2021-11-20 17:01:10 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+Website = os.getenv("Website") #Set to 'true' if you're hosting bot in replit and if you're hosting your bot in heroku set it to anything else except 'true'.
+
+StatusContent1 = "" #First status content
+
+StatusContent2 = "" #Second status content
+
+Token = os.getenv("Token")
